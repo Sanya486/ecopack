a222eea56c0bf14be1085358a5938ea8f9cef5fb
Sanya486 <120394126+Sanya486@users.noreply.github.com>
2023-09-11 10:56:04 +0000

    Deploying to gh-pages from @ Sanya486/ecopack@6e22842eba6defed85c9321077882fc15dcb48b5 🚀

---
+++ about.477c1884.js
@@ -0,0 +1,3 @@
+!function(){let e={burgerIcon:document.querySelector(".burger-icon"),closeIcon:document.querySelector(".icon-x"),modalmenu:document.querySelector(".modal-menu"),body:document.querySelector("body")};e.burgerIcon.addEventListener("click",()=>{e.modalmenu.classList.remove("is-hidden"),e.body.style.overflow="hidden"}),e.closeIcon.addEventListener("click",()=>{e.modalmenu.classList.add("is-hidden"),e.body.style.overflow="scroll"})}();//# sourceMappingURL=about.477c1884.js.map
+
+//# sourceMappingURL=about.477c1884.js.map
---
+++ about.7805e1c1.js
@@ -0,0 +1,3 @@
+const e={burgerIcon:document.querySelector(".burger-icon"),closeIcon:document.querySelector(".icon-x"),modalmenu:document.querySelector(".modal-menu"),body:document.querySelector("body")};e.burgerIcon.addEventListener("click",()=>{e.modalmenu.classList.remove("is-hidden"),e.body.style.overflow="hidden"}),e.closeIcon.addEventListener("click",()=>{e.modalmenu.classList.add("is-hidden"),e.body.style.overflow="scroll"});//# sourceMappingURL=about.7805e1c1.js.map
+
+//# sourceMappingURL=about.7805e1c1.js.map
